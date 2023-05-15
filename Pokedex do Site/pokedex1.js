752ad67222d992052afaf01e641c861ec1c1ce68
Cabayer915 <cauaribeirob915@gmail.com>
2023-05-14 22:04:35 -0300

    primeira versão da Pokedex

---
+++ Pokedex do Site/pokedex1.js
@@ -0,0 +1,17 @@
+// const botaoAlterarTema = document.getElementById("logotipo");
+
+// const corpo = document.querySelector("body");
+
+// const imagemBotao = document.querySelector("logotipo");
+
+// botaoAlterarTema.addEventListener("click", () => {
+//     const modoEscuroAtivo = corpo.classList.contains("tema-escuro");
+//     /* esta dando ERRO imagemBotao.setAttribute("src","./PKC.png") */
+//     if (modoEscuroAtivo){
+//       corpo.classList.remove("tema-escuro");
+//       /* esta dando ERRO imagemBotao.setAttribute("src","./PKC.png") */
+//     } else {
+//         corpo.classList.add("tema-escuro");
+//         /* esta dando ERRO imagemBotao.setAttribute("src","./PKC.png") */
+//     }
+// });
